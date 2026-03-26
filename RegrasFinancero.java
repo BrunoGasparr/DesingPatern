5f79d2743811d6af9c71e9a41079b36f0a875dd1
TIagoF1819 <tiagofreire@edu.unifil.br>
2026-03-26 19:00:47 -0300

    Atualizando a interface "RegraFinanceiro"
    Adicionei funcionalidades a interface que estava faltando e fiz uma documentação com comentários no código. Adicionado também as funcionalidades abaixo.     
                                                   
         Aplica um desconto ao valor bruto
         Calcula o valor total final (Bruto - Descontos + Taxas)
         Mostra o resumo das operações financeiras realizadas
         Retorna o lucro líquido (Bruto - Desconto - Taxas - Impostos)
         Realiza o estorno de um valor específico ( Valor total final + saldo )

--- RegrasFinancero.java
+++ RegrasFinancero.java
@@ -1,6 +1,20 @@
-package DesingPatern.DesingPatern;
-public interface RegrasFinancero {
-    void descontoPix(double valor);
-    void descontoAvista(double valor);
-    
+package DesingPatern;
+
+public interface RegraFinanceiro {
+
+    // Aplica um desconto ao valor bruto
+    double calcularDesconto(double valorBruto, double percentual);
+
+    // Calcula o valor total final (Bruto - Descontos + Taxas)
+    double calcularValorTotal(double valorBruto, double desconto, double taxas);
+
+    // Mostra o resumo das operações financeiras realizadas
+    void exibirResumoFinanceiro();
+
+    // Retorna o lucro líquido (Bruto - Desconto - Taxas - Impostos)
+    double calcularLucroLiquido();
+
+    // Realiza o estorno de um valor específico (n sei oque fazer mais)
+    void realizarEstorno(String transacaoId, double valor);
+
 }
